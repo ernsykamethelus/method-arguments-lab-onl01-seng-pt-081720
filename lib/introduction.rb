b720214c3a153c7f8740f5668f3c55afc2b97f01
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-20 02:24:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,9 @@
-def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
+# lib/introduction.rb
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+# lib/introduction_with_language.rb
+  def introduction_with_language(name, language)
+    puts"Hi, my name is #{name} and i am learning to program in #{language}."
 end
